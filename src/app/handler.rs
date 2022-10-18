9e4fe37f8bd035f5e97df4cd77b7ee2867546356
Gael-devv <69738055+Gael-devv@users.noreply.github.com>
2022-10-17 23:56:40 -0500

    Changes in the forecast subcommand and addition of the history subcommand implementation

--- src/app/handler.rs
+++ src/app/handler.rs
@@ -5,14 +5,13 @@ use std::{
 };
 
 use weer_api::{*, chrono::{Utc, TimeZone, Timelike}};
-use super::{App, cli::tables};
 use clap::ArgMatches;
 use colored::*;
+use super::{App, tables};
 
 
 pub fn matches_handler(app: &App) -> Result<(), Box<dyn Error>> {
     let out = io::stdout();
-    // let err = io::stderr();
 
     let q = {
         if let Some(ip) = app.matches.get_one::<IpAddr>("ip") {
@@ -28,9 +27,9 @@ pub fn matches_handler(app: &App) -> Result<(), Box<dyn Error>> {
 
     match app.matches.subcommand() {
         Some(("forecast", sub_m)) => forecast(sub_m, &out, &app, q),
-        Some(("history", _sub_m)) => Ok(()),
-        Some(("search", _sub_m)) => Ok(()),
-        Some(("future", _sub_m)) => Ok(()),
+        Some(("history", sub_m)) => history(sub_m, &out, &app, q),
+        // Some(("search", _sub_m)) => todo!(),
+        // Some(("future", _sub_m)) => todo!(),
         Some((&_, _)) => todo!(),
         None => Ok(())
     }
@@ -39,32 +38,27 @@ pub fn matches_handler(app: &App) -> Result<(), Box<dyn Error>> {
 fn forecast(sub_m: &ArgMatches, out: &io::Stdout, app: &App, q: Query) -> Result<(), Box<dyn Error>> {
     let mut out = out.lock();
 
-    let days = sub_m.get_one::<String>("days").unwrap().parse::<u8>()?;
-    let hour = sub_m.get_one::<String>("hour").unwrap_or(&"0".to_string()).parse::<u32>()?;
+    let days = sub_m.get_one::<u8>("days").unwrap();
+    let hour = sub_m.get_one::<u32>("hour").unwrap_or(&0);
     let aqi = sub_m.get_one::<bool>("aqi").unwrap();
     let alerts = sub_m.get_one::<bool>("alerts").unwrap();
+
     let dt = if let Some(dt) = sub_m.get_one::<String>("dt") {
-        Utc.datetime_from_str(dt, "%Y-%m-%d")?.with_hour(hour).unwrap()
+        Utc.datetime_from_str(dt, "%Y-%m-%d")?.with_hour(hour.clone()).unwrap()
     } else {
-        Utc::now().with_hour(hour).unwrap()
+        Utc::now().with_hour(hour.clone()).unwrap()
     };
 
-    let mut req = app.client.forecast()
-        .query(q)
+    let mut req = app.client.forecast();
+    req.query(q)
         .dt(dt)
-        .days(days)
-        .hour();
+        .days(days.clone())
+        .hour(true)
+        .aqi(*aqi)
+        .alerts(*alerts);
 
     if let Some(lang) = app.cfg.lang() {
-        req = req.lang(lang);
-    }
-
-    if *aqi {
-        req = req.aqi();
-    }
-
-    if *alerts {
-        req = req.alerts();
+        req.lang(lang);
     }
 
     let resp = req.call()?;
@@ -75,6 +69,39 @@ fn forecast(sub_m: &ArgMatches, out: &io::Stdout, app: &App, q: Query) -> Result
     writeln!(out, "{}:", "Current".bold())?;
     tables::current_table(resp.current)?.print(&mut out)?;
     
+    for fd in resp.forecast.forecast_day.iter() {
+        writeln!(out, "{} - {}:", "Forecast".bold(), fd.date.italic())?;
+        let table = tables::forecastday_table(fd)?;
+        table.print(&mut out)?;
+    }
+
+    Ok(())
+}
+
+fn history(sub_m: &ArgMatches, out: &io::Stdout, app: &App, q: Query) -> Result<(), Box<dyn Error>> {
+    let mut out = out.lock();
+
+    let hour = sub_m.get_one::<u32>("hour").unwrap_or(&0);
+    let dt = if let Some(dt) = sub_m.get_one::<String>("dt") {
+        Utc.datetime_from_str(dt, "%Y-%m-%d")?.with_hour(hour.clone()).unwrap()
+    } else {
+        Utc::now().with_hour(hour.clone()).unwrap()
+    };
+
+    let mut req = app.client.history();
+    req.query(q)
+        .dt(dt)
+        .hour(true);
+    
+    if let Some(end_dt) = sub_m.get_one::<String>("end_dt") {
+        req.end_dt(Utc.datetime_from_str(end_dt, "%Y-%m-%d")?);
+    }
+
+    let resp = req.call()?;
+
+    writeln!(out, "{}:", "Location".bold())?;
+    tables::location_table(resp.location).print(&mut out)?;
+
     for fd in resp.forecast.forecast_day.iter() {
         writeln!(out, "{} - {}:", "Forecast".bold(), fd.date.italic())?;
         let table = tables::forecastday_table(fd)?;
